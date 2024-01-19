c05c117bdf48986196291c77ccbc5bdc7ac2ae2c
ivanberez <berejnoyi@mail.ru>
2024-01-19 16:06:19 +0800

    Revert "Add transformations scripts"
    This reverts commit 10b1cc519526358bc31270fdc3a006a58f9d4f3a.

---
+++ Assets/Scripts/SpherMover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherMover : MonoBehaviour
+{
+    [SerializeField] private float _speedMove;
+
+    
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        
+    }
+}
